706ab87e156b1a154065314fcd61b74d25e6fbf0
alex555roselyn <corado502@gmail.com>
2020-05-21 14:10:48 -0600

    changes

--- sisoft/src/Dashboard.js
+++ sisoft/src/Dashboard.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import  {Button} from 'react-bootstrap';
 
-export default class Dashboard extends React.Component{
+export default class Dashboard extends React.Component {
 
-/*    <Step status="finish" title="Login" icon={<UserOutlined />} />
-    <Step status="finish" title="Verification" icon={<SolutionOutlined />} />
-    <Step status="process" title="Pay" icon={<LoadingOutlined />} />
-    <Step status="wait" title="Done" icon={<SmileOutlined />} />*/
-render(){
-	return(
-		<>
-    Hola prros
-		</>
-		);
-}
 
+  render() {
+    return (
+      <>
+     <Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
+      </>
+         );
+  }
 }
--- sisoft/src/Menu.js
+++ sisoft/src/Menu.js
@@ -22,7 +22,10 @@ import {
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Dashboard from './Dashboard.js';
 import Meseros from './Home.js';
-
+import Listar from './Listar.js';
+import Mantenimiento from './Mantenimiento.js';
+import RenderPDF from './RenderPDF.js';
+import OrdenesCompra from './OrdenesCompra.js';
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
@@ -53,29 +56,29 @@ export default class Menus extends React.Component {
             <SubMenu key="sub1" icon={<HeartOutlined />} title="Productos">
               <Menu.Item key="3" icon={<SettingOutlined />}>Mantenimiento <Link to="/mantenimiento" /></Menu.Item>
               <Menu.Item key="4" icon={<OrderedListOutlined />}>Listar<Link to="/listar&prod" /></Menu.Item>
-              <Menu.Item key="5" icon={<FormOutlined />}>Modificar<Link to="/modificar&prod" /></Menu.Item>
             </SubMenu>
             <SubMenu key="sub2" icon={<ShoppingCartOutlined />} title="Pedidos">
-              <Menu.Item key="6" icon={<OrderedListOutlined />}>Ordenes<Link to="/ordenes" /></Menu.Item>
-              <Menu.Item key="8" icon={<EditOutlined />}>Nuevo<Link to="/ordenes&nuevo" /></Menu.Item>
-              <Menu.Item key="9" icon={<ExpandAltOutlined />}>Graficas<Link to="/graficas" /></Menu.Item>
+              <Menu.Item key="5" icon={<OrderedListOutlined />}>Ordenes<Link to="/ordenes" /></Menu.Item>
+              <Menu.Item key="6" icon={<EditOutlined />}>Nuevo<Link to="/ordenes&nuevo" /></Menu.Item>
+              <Menu.Item key="7" icon={<ExpandAltOutlined />}>Graficas<Link to="/graficas" /></Menu.Item>
             </SubMenu>
                 <SubMenu key="sub3" icon={<TeamOutlined />} title="Clientes">
-              <Menu.Item key="10" icon={<AuditOutlined />}>Listar<Link to="/listar&clientes" /></Menu.Item>
-              <Menu.Item key="11" icon={<EditOutlined />}>Nuevo<Link to="/clientes&nuevo" /></Menu.Item>
+              <Menu.Item key="8" icon={<AuditOutlined />}>Listar<Link to="/listar&clientes" /></Menu.Item>
+              <Menu.Item key="9" icon={<EditOutlined />}>Nuevo<Link to="/clientes&nuevo" /></Menu.Item>
+                <Menu.Item key="10" icon={<FormOutlined />}>Modificar<Link to="/modificar&clientes" /></Menu.Item>
             </SubMenu>
                <SubMenu key="sub4" icon={<ContainerOutlined />} title="Inventario">
-              <Menu.Item key="12" icon={<OrderedListOutlined />}>Listar<Link to="/inventario" /></Menu.Item>
+              <Menu.Item key="11" icon={<OrderedListOutlined />}>Listar<Link to="/inventario" /></Menu.Item>
             </SubMenu>
 
             <SubMenu key="sub5" icon={<FileOutlined />} title="Facturación">
-              <Menu.Item key="13" icon={<OrderedListOutlined />}>Listar<Link to="/listar&facturas" /></Menu.Item>
-              <Menu.Item key="14" icon={<EditOutlined />}>Nueva<Link to="/factura&nuevo" /></Menu.Item>
+              <Menu.Item key="12" icon={<OrderedListOutlined />}>Listar<Link to="/listar&facturas" /></Menu.Item>
+              <Menu.Item key="13" icon={<EditOutlined />}>Nueva<Link to="/factura&nuevo" /></Menu.Item>
             </SubMenu>
                <SubMenu key="sub6" icon={<PrinterOutlined />} title="Informes">
-              <Menu.Item key="15" icon={<FilePdfOutlined />}>Info de Clientes y Descuentos<Link to="/informe&clientes" /></Menu.Item>
-              <Menu.Item key="16" icon={<FilePdfOutlined />}>Info de Productos<Link to="/informe&productos" /></Menu.Item>
-              <Menu.Item key="17" icon={<FilePdfOutlined />}>Info de Ventas<Link to="/informe&ventas" /></Menu.Item>
+              <Menu.Item key="14" icon={<FilePdfOutlined />}>Info de Clientes y Descuentos<Link to="/informe&clientes" /></Menu.Item>
+              <Menu.Item key="15" icon={<FilePdfOutlined />}>Info de Productos<Link to="/informe&productos" /></Menu.Item>
+              <Menu.Item key="16" icon={<FilePdfOutlined />}>Info de Ventas<Link to="/informe&ventas" /></Menu.Item>
             </SubMenu>
             
           </Menu>
@@ -84,13 +87,17 @@ export default class Menus extends React.Component {
           <Header className="site-layout-background" style={{ padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Inicio</Breadcrumb.Item>
-              <Breadcrumb.Item>Hola</Breadcrumb.Item>
+              <Breadcrumb.Item>SISOFT</Breadcrumb.Item>
+              
             </Breadcrumb>
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
 
      <Route exact path="/" component={Dashboard} />
-      <Route path="/meseros" component={Meseros} />
+      <Route path="/mantenimiento" component={Mantenimiento} />
+      <Route path="/listar&prod" component={Listar} />
+       <Route path="/ordenes&nuevo" component={OrdenesCompra} />
+         <Route path="/informe&ventas" component={RenderPDF} />
+      
 
             </div>
           </Content>
